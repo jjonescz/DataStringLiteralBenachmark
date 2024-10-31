136382e0403b079bcf9bf3a7634f0d6b581b7736
Jan Jones <janjones@microsoft.com>
2024-10-31 11:45:09 +0100

    Benchmark short string

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// dotnet run -c Release --filter "*"
+
+using System.Runtime.CompilerServices;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkSwitcher.FromAssembly(typeof(Tests).Assembly).Run(args);
+
+[ShortRunJob]
+[DisassemblyDiagnoser(maxDepth: 0)]
+public class Tests
+{
+    [Benchmark(Baseline = true)]
+    public void Ldstr() => TakeString("test");
+
+    [Benchmark]
+    public void Ldsfld() => TakeString(Generated.s_test);
+
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    static void TakeString(string s) { }
+}
+
+static class Generated
+{
+    public static readonly string s_test = Encoding.UTF8.GetString("test"u8);
+}
